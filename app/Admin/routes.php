32202c88d8b41ed9bf3f51d04f64736bc235b348
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2023-10-03 14:58:14 +0300

    new changes

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -25,6 +25,8 @@ Route::group([
     $router->resource('tasks', TaskController::class);
     $router->resource('events', EventController::class);
     $router->get('/calendar', 'HomeController@calendar')->name('calendar');
+    $router->resource('patients', PatientController::class); 
+    $router->resource('patient-records', PatientRecordController::class); 
 
     $router->resource('gens', GenController::class);
 });
