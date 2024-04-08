a0219cdf69c53c76cc94db8b1c77c45cd6a8d5b3
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2024-04-08 13:11:27 +0300

    Update filesystems.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -57,7 +57,7 @@ return [
         'admin' => [
             'driver' => 'local',
             'root' => public_path('storage'),
-            'url' => 'storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
         ],
     ],
