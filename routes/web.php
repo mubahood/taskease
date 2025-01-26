0f2b88ff1de6e323f159b9075bf6f0148617e0e3
Muhindo Mubaraka <mubahood360@gmail.com>
2025-01-26 10:28:29 +0300

    Add routes for cache clearing and database migration

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,35 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 
+Route::get('/clear', function () {
+
+    Artisan::call('cache:clear');
+    Artisan::call('config:clear');
+    Artisan::call('config:cache');
+    Artisan::call('view:clear');
+
+    return "Cleared!";
+});
+Route::get('migrate', function () {
+    //run laravel migrate command in code
+    $RESP = Artisan::call('migrate');
+    echo "<pre>";
+    print_r($RESP);
+    echo "</pre>";
+    die();
+});
+
+Route::get('/migrate-refresh', function () {
+    //run laravel migrate command in code
+    $RESP = Artisan::call('migrate:refresh');
+    echo "<pre>";
+    print_r($RESP);
+    echo "</pre>";
+    die();
+});
+
+
+
 Route::get('app', function () {
     //return url('taskease-v1.apk');
     return redirect(url('taskease-v1.apk'));
